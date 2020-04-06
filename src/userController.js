1e8d2a1041c8005082ea8449bea263837f00d023
Phillwich <P.Hellwich@gmx.de>
2020-04-06 09:07:35 +0200

    fs read/writeFile

--- src/userController.js
+++ src/userController.js
@@ -1,36 +1,46 @@
-let userDummyData = require('./userModel')
+const fs = require('fs')
 
-const addUser = (req, res, next) => {
+const addUser = async (req, res, next) => {
   if (!req.body.user) return res.status(400).send('Missing user data')
   const { user } = req.body
-  userDummyData.push(user)
+  const dummyData = JSON.parse(await fs.readFileSync(__dirname + "/dummyData.json", "utf8"))
+  dummyData.user.push(user)
+  await fs.writeFileSync(__dirname + '/dummyData.json', JSON.stringify(dummyData))
   return res.status(200).send('Successfull added user')
 }
 
-const deleteUser = (req, res, next) => {
-  if (!req.query) return res.status(400).send('Missing userId in query parameters')
+const deleteUser = async (req, res, next) => {
+  if (!req.query.userId) return res.status(400).send('Missing userId in query parameters')
   const { userId } = req.query
-  const index = userDummyData.findIndex(dummyUser => dummyUser.id === userId)
-  userDummyData.splice(index, 1)
-  return res.status(200).send(userDummyData)
+  const dummyData = JSON.parse(await fs.readFileSync(__dirname + "/dummyData.json", "utf8"))
+  const index = dummyData.user.findIndex(dummyUser => dummyUser.id === userId)
+  dummyData.user.splice(index, 1)
+  await fs.writeFileSync(__dirname + '/dummyData.json', JSON.stringify(dummyData))
+  return res.status(200).send(dummyData.user)
 }
 
-const updateUser = (req, res, next) => {
+const updateUser = async (req, res, next) => {
   if (!req.body.user) return res.status(400).send('Missing user data')
   const { user } = req.body
-  const index = userDummyData.findIndex(dummyUser => dummyUser.id === user.id)
-  userDummyData[index] = user
+  const dummyData = JSON.parse(await fs.readFileSync(__dirname + "/dummyData.json", "utf8"))
+  const index = dummyData.user.findIndex(dummyUser => dummyUser.id === user.id)
+  dummyData.user[index] = user
+  await fs.writeFileSync(__dirname + '/dummyData.json', JSON.stringify(dummyData))
   return res.status(200).send('Successfull updated user')
 }
 
-const getUser = (req, res, next) => {
+const getUser = async (req, res, next) => {
   // If userId in query parameters return specific user otherwise all users
-  if (req.query) {
+  const dummyData = JSON.parse(await fs.readFileSync(__dirname + "/dummyData.json", "utf8"))
+  let response 
+  if (req.query.userId) {
+    console.log(req.query)
     const { userId } = req.query
-    const index = userDummyData.findIndex(dummyUser => dummyUser.id === userId)
-    userDummyData = userDummyData.splice(index, 1)
+    const index = dummyData.user.findIndex(dummyUser => dummyUser.id === userId)
+    response = dummyData.user.splice(index, 1)
+    console.log(response)
   }
-  return res.status(200).send(userDummyData)
+  return res.status(200).send(response ? response : dummyData.user)
 }
 
 module.exports = {
